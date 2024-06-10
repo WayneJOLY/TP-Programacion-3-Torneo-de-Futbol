75d19d4d01b127b660f5be208aee5ac7ecee350c
Gerald <geraldjoly191@gmail.com>
2024-06-10 13:23:45 -0300

    Implementé la clase CJugador con los atriutos que pienso que tendrá , no implemené metodos

---
+++ CEquipo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTORNEO_FUTBOL
+{
+    internal class CEquipo
+    {
+        private string nombre;
+        private string codigo;
+        private List<string> colores;
+        private LinkedList<CJugador> jugadores;
+        private CEntrenador entrenador;
+
+        public CEquipo(string nombre, string codigo, LinkedList<string> colores)
+        {
+            this.nombre = nombre;
+            this.codigo = codigo;
+            this.colores = new List<string>(colores);
+        }
+    }
+}
