541ee69afa6ee3f3951be1cab1fdae25d7d759d5
Gerald <geraldjoly191@gmail.com>
2024-06-23 17:48:29 -0300

    Saqué los codigos de esa

--- Program.cs
+++ Program.cs
@@ -1,10 +1,1 @@
-﻿namespace CTORNEO_FUTBOL
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿
